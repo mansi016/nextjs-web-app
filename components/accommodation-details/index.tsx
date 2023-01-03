1b443e78a7251a272e4af5840fe7e58fde409e00
mansidonga16 <mansi167.rejoice@gmail.com>
2023-01-03 18:32:15 +0530

    fixed router query issue

--- components/accommodation-details/index.tsx
+++ components/accommodation-details/index.tsx
@@ -1,13 +1,16 @@
+import { useRouter } from "next/router";
+import { it } from "node:test";
 import Autocomplete from "react-google-autocomplete";
 
 export default function AccommodationDetails() {
+  const router = useRouter();
+  const pid = router.query;
+
   return (
     <>
       <div>
-        <h2>Welcome to accommodation details</h2>
-        <div>
-          
-        </div>
+        <h2>Welcome to accommodation details of {pid?.slug}</h2>
+        <h2> {pid?.slug}</h2>
       </div>
     </>
   );
--- components/accommodation/index.tsx
+++ components/accommodation/index.tsx
@@ -1,15 +1,30 @@
 import Autocomplete from "react-google-autocomplete";
+import { useRouter } from "next/router";
 
 export default function Accommodation() {
+  const router = useRouter();
+
   return (
     <>
       <div>
         <h2>Welcome to accommodation</h2>
         <div>
-          <Autocomplete
+          {/* <Autocomplete
             apiKey="ABcsdfojwdDOSFNOSnowiwbnf" // place your google map api key here
             onPlaceSelected={(place) => console.log(place)}
-          />
+          /> */}
+          <select
+            onChange={(e) => {
+              router.push(`/student-accommodation/${e.target.value}`);
+            }}
+          >
+            <option value="" disabled selected>
+              ---Select Place---
+            </option>
+            <option value="London">London</option>
+            <option value="India">India</option>
+            <option value="NewYork">New York</option>
+          </select>
         </div>
       </div>
     </>
